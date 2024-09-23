af19ccb8155d8b7ce5c4d9c1becd14573f4c01e2
shahnavazkhan <bca2023shahnavazkhan1852@tnraocollege.org>
2024-09-23 19:18:37 +0530

    removed node_modules

--- Backend/Routes/auth.route.js
+++ Backend/Routes/auth.route.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 import signup from '../controllers/auth_controllers/signup.controller.js';
 
-const authRoutes = () => {
+
 router.get('/signup', signup);
 
-}
-export default authRoutes;
+
+export default router;
